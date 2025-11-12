ac71fe36c6f016389a3607f65c29f20f0a0ba4e4
Deploy from CI <>
2025-11-12 13:58:52 +0000

    Deploy 559a6b43a5716b2afaf2a5011bd542a81f63463f to gh-pages

---
+++ toc.js
@@ -0,0 +1,70 @@
+// Populate the sidebar
+//
+// This is a script, and not included directly in the page, to control the total size of the book.
+// The TOC contains an entry for each page, so if each page includes a copy of the TOC,
+// the total size of the page becomes O(n**2).
+class MDBookSidebarScrollbox extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="index.html">Introductions</a></li><li class="chapter-item expanded affix "><li class="part-title">Kubernetes</li><li class="chapter-item expanded "><a href="kube/index.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="kube/installation.html"><strong aria-hidden="true">1.1.</strong> Installation</a></li><li class="chapter-item expanded "><a href="kube/local_setup.html"><strong aria-hidden="true">1.2.</strong> Local Setup</a></li><li class="chapter-item expanded "><a href="kube/add_worker.html"><strong aria-hidden="true">1.3.</strong> Add Worker Node</a></li><li class="chapter-item expanded "><a href="kube/install_metallb.html"><strong aria-hidden="true">1.4.</strong> Add a Load Balancer</a></li><li class="chapter-item expanded "><a href="kube/traefik_setup.html"><strong aria-hidden="true">1.5.</strong> Setup Traefik</a></li><li class="chapter-item expanded "><a href="kube/nfs_provisioner.html"><strong aria-hidden="true">1.6.</strong> NFS Provisioner</a></li><li class="chapter-item expanded "><a href="kube/metrics_server.html"><strong aria-hidden="true">1.7.</strong> Metrics Server</a></li><li class="chapter-item expanded "><a href="kube/certmanager.html"><strong aria-hidden="true">1.8.</strong> Certificate Manager</a></li><li class="chapter-item expanded "><a href="kube/cnpg.html"><strong aria-hidden="true">1.9.</strong> PostgreSQL</a></li></ol></li><li class="chapter-item expanded "><li class="part-title">Linux</li><li class="chapter-item expanded "><a href="linux/index.html"><strong aria-hidden="true">2.</strong> Introduction</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="linux/raid_setup.html"><strong aria-hidden="true">2.1.</strong> RAID Setup</a></li><li class="chapter-item expanded "><a href="linux/raid_removal.html"><strong aria-hidden="true">2.2.</strong> RAID Destruction</a></li><li class="chapter-item expanded "><a href="linux/publish_host.html"><strong aria-hidden="true">2.3.</strong> Custom Hostnames</a></li><li class="chapter-item expanded "><a href="linux/prometheus.html"><strong aria-hidden="true">2.4.</strong> Prometheus</a></li><li class="chapter-item expanded "><a href="linux/alertmanager.html"><strong aria-hidden="true">2.5.</strong> AlertManager</a></li><li class="chapter-item expanded "><a href="linux/node_exporter.html"><strong aria-hidden="true">2.6.</strong> Node Exporter</a></li><li class="chapter-item expanded "><a href="linux/cassandra_installation.html"><strong aria-hidden="true">2.7.</strong> Cassandra Installation</a></li></ol></li><li class="chapter-item expanded "><li class="part-title">The Hard Way</li><li class="chapter-item expanded "><a href="hard_way/index.html"><strong aria-hidden="true">3.</strong> Introduction</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="hard_way/n8n_install_with_postgresql.html"><strong aria-hidden="true">3.1.</strong> N8N the hard way</a></li><li class="chapter-item expanded "><a href="hard_way/network_storage.html"><strong aria-hidden="true">3.2.</strong> Network Storage</a></li><li class="chapter-item expanded "><a href="hard_way/network_storage_iscsi.html"><strong aria-hidden="true">3.3.</strong> Network Storage - iSCSI</a></li></ol></li><li class="chapter-item expanded "><li class="part-title">Runbooks</li><li class="chapter-item expanded "><a href="runbooks/index.html"><strong aria-hidden="true">4.</strong> Introduction</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="runbooks/postgresql_create_database_user.html"><strong aria-hidden="true">4.1.</strong> PostgreSQL Database/User Creation</a></li></ol></li></ol>';
+        // Set the current, active page, and reveal it if it's hidden
+        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
+        if (current_page.endsWith("/")) {
+            current_page += "index.html";
+        }
+        var links = Array.prototype.slice.call(this.querySelectorAll("a"));
+        var l = links.length;
+        for (var i = 0; i < l; ++i) {
+            var link = links[i];
+            var href = link.getAttribute("href");
+            if (href && !href.startsWith("#") && !/^(?:[a-z+]+:)?\/\//.test(href)) {
+                link.href = path_to_root + href;
+            }
+            // The "index" page is supposed to alias the first chapter in the book.
+            if (link.href === current_page || (i === 0 && path_to_root === "" && current_page.endsWith("/index.html"))) {
+                link.classList.add("active");
+                var parent = link.parentElement;
+                if (parent && parent.classList.contains("chapter-item")) {
+                    parent.classList.add("expanded");
+                }
+                while (parent) {
+                    if (parent.tagName === "LI" && parent.previousElementSibling) {
+                        if (parent.previousElementSibling.classList.contains("chapter-item")) {
+                            parent.previousElementSibling.classList.add("expanded");
+                        }
+                    }
+                    parent = parent.parentElement;
+                }
+            }
+        }
+        // Track and set sidebar scroll position
+        this.addEventListener('click', function(e) {
+            if (e.target.tagName === 'A') {
+                sessionStorage.setItem('sidebar-scroll', this.scrollTop);
+            }
+        }, { passive: true });
+        var sidebarScrollTop = sessionStorage.getItem('sidebar-scroll');
+        sessionStorage.removeItem('sidebar-scroll');
+        if (sidebarScrollTop) {
+            // preserve sidebar scroll position when navigating via links within sidebar
+            this.scrollTop = sidebarScrollTop;
+        } else {
+            // scroll sidebar to current active section when navigating via "next/previous chapter" buttons
+            var activeSection = document.querySelector('#sidebar .active');
+            if (activeSection) {
+                activeSection.scrollIntoView({ block: 'center' });
+            }
+        }
+        // Toggle buttons
+        var sidebarAnchorToggles = document.querySelectorAll('#sidebar a.toggle');
+        function toggleSection(ev) {
+            ev.currentTarget.parentElement.classList.toggle('expanded');
+        }
+        Array.from(sidebarAnchorToggles).forEach(function (el) {
+            el.addEventListener('click', toggleSection);
+        });
+    }
+}
+window.customElements.define("mdbook-sidebar-scrollbox", MDBookSidebarScrollbox);
